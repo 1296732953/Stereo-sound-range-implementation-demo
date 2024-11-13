59250f92cd96986f0f3d4f9937e6ab6b55aad515
浮止 <1296732953@qq.com>
2024-11-13 19:59:29 +0800

    Updates

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import SpatialAudioSimulation from "@/components/spatial-audio-simulation";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SpatialAudioSimulation />
-    </main>
-  );
-}
